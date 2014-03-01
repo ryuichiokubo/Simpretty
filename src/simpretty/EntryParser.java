61c03868cbf626c5881bbbfa46425614644e68b6
Ryuichi Okubo <ryuichi.okubo.1986@gmail.com>
2014-03-01 23:11:07 +0200

    Replaced yahoo rss with teamtreehouse rss. Logic to fetch article detail with thread and html parsing is deleted.

--- src/simpretty/EntryParser.java
+++ src/simpretty/EntryParser.java
@@ -1,84 +1,30 @@
 package simpretty;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.TextNode;
-import org.jsoup.select.Elements;
-
 import com.sun.syndication.feed.synd.SyndEntry;
 
-public class EntryParser implements Runnable{
-	// XXX yahoo parser for now
-	
+public class EntryParser {
 	private static final Logger log = Logger.getLogger(SimprettyServlet.class.getName());
-	private static final int CON_TIMEOUT = 20 * 1000;
 
 	private SyndEntry entry;
-	private HashMap<String, String> contents = new HashMap<String, String>(); // XXX TreeMap
+	private HashMap<String, String> contents = new HashMap<String, String>();
 
 	public EntryParser(SyndEntry entry) {
 		this.entry = entry;
 	}
 	
-	public void run() {
+	public void parse() {
 		log.info("Started parsing.");
 		
 		contents.put("title", entry.getTitle());
 		contents.put("hash", Integer.toString(entry.hashCode()));
 		contents.put("time", Long.toString(entry.getPublishedDate().getTime()));
-		try {
-			parseUrl(entry.getLink());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		contents.put("link", entry.getLink());
 	}
 	
 	public HashMap<String, String> getContents() {
 		return contents;
-	}
-		
-	private void parseUrl(String url) throws IOException {
-		if (url.startsWith("http://rd.yahoo.co.jp")) {
-			url = url.substring(url.indexOf("*")+1);
-		}
-		Document document = Jsoup.connect(url).timeout(CON_TIMEOUT).get();
-		contents.put("link", findDetailUrl(document));
-		contents.put("summary", findSummary(document));
-	}
-	
-	private String findSummary(Document document) {
-		String summary = "";
-		for (TextNode node: document.select("div#detailHeadline").first().textNodes()) {
-			if (!node.isBlank()) {
-				summary += node.text();
-			}
-			if (summary.length() > 0) break;
-		}
-		
-		return summary;		
-	}
-
-	private String findDetailUrl(Document document) {
-		Elements links = document.select("div#detailHeadline  a[href]");
-		String href = "";
-		ArrayList<String> tmpArr = new ArrayList<String>();
-		for (Element link : links) {
-			href = link.attr("href");
-			if (href.startsWith("http://headlines.yahoo.co.jp")) { // XXX got http://backnumber.dailynews.yahoo.co.jp/?m=7700474&e=food_service_industry
-				if (tmpArr.contains(href)) {
-					break;
-				} else {
-					tmpArr.add(href);
-				}
-			}
-		}
-		return href;		
 	}
 }
--- src/simpretty/SimprettyServlet.java
+++ src/simpretty/SimprettyServlet.java
@@ -6,13 +6,11 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.ThreadFactory;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
-import com.google.appengine.api.ThreadManager;
 import com.google.gson.Gson;
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
@@ -48,27 +46,11 @@ public class SimprettyServlet extends HttpServlet {
 	
 	private List<HashMap<String, String>> parseFeed(SyndFeed feed) {
 		List<HashMap<String, String>> articles 	= new ArrayList<HashMap<String, String>>();
-		List<EntryParser> parsers 				= new ArrayList<EntryParser>();
-		List<Thread> threads 					= new ArrayList<Thread>();
-		ThreadFactory threadFactory 			= ThreadManager.currentRequestThreadFactory();
 		
 		for (Object obj : feed.getEntries()) {
 			SyndEntry entry = (SyndEntry) obj;
 			EntryParser parser = new EntryParser(entry);
-			Thread thread = threadFactory.newThread(parser);
-			thread.start();
-			threads.add(thread);
-			parsers.add(parser);
-		}
-		for (Thread thread: threads) {
-			try {
-				thread.join(); // XXX replace with notify or something to wait for threads execution smartly
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		for (EntryParser parser: parsers) {
+			parser.parse();
 			articles.add(parser.getContents());
 		}
 		
@@ -85,9 +67,9 @@ public class SimprettyServlet extends HttpServlet {
 	}
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException, ServletException { // XXX exception handling
+			throws IOException, ServletException {
 		
-		SyndFeed feed = getFeedFromUrl("http://rss.dailynews.yahoo.co.jp/fc/rss.xml");
+		SyndFeed feed = getFeedFromUrl("http://feeds.feedburner.com/teamtreehouse");
 		List<HashMap<String, String>> articles = parseFeed(feed);	
 		sendResponse(articles, resp);
 	}
