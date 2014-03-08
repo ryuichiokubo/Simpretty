cdbe8c968d178d1eec0f0094ce8f1b27b6271fd4
Ryuichi Okubo <ryuichi.okubo.1986@gmail.com>
2014-03-08 15:50:00 +0200

    Client: use script tag for template

--- war/js/index.js
+++ war/js/index.js
@@ -1,60 +1,71 @@
-/*jslint browser:true */
+/*jslint browser:true, indent:4 */
+/*global DOMParser*/
 
 (function () {
     "use strict";
-  
+
     var ryuichiokubo_simpretty = function () {
 
-        var URL_FEED = 'http://localhost:8888/simpretty',
         //var URL_FEED = 'http://simpretty-rss.appspot.com/simpretty';
+        var URL_FEED = 'http://localhost:8888/simpretty',
+            el = {},
+            tmpl = {},
+            renderFeed,
+            getFeed;
 
-            // HTML elements
-            el = (function () {
-                var main = document.getElementById('main'),
-                    article = document.getElementsByTagName('article')[0];
+        // HTML elements
+        el = (function () {
+            var main = document.getElementById('main');
 
-                return {
-                    main: main,
-                    article: article
-                };
-            }()),
+            return {
+                main: main
+            };
+        }());
 
-            renderFeed = function (data) {
+        // HTML templates
+        tmpl = function (id) {
+            var wrapper = document.createElement('div');
+            wrapper.innerHTML = document.getElementById(id).firstChild.textContent.replace('\n', '', 'g');
+            return wrapper.firstElementChild.cloneNode();
+        };
 
-                // Larger fonts for more comments
-                var getFontSize = function (feed) {
-                    var size = 1,
-                        unit = 'rem',
-                        extra = feed.comment * 0.1;
-                    return size + extra + unit;
-                },
+        renderFeed = function (data) {
 
-                    // Append feeds which are sent from server as sorted list
-                    feeds = JSON.parse(data);
-              
-                feeds.forEach(function (feed) {
-                    var node = el.article.cloneNode(),
-                        header = node.firstElementChild,
-                        a = header.firstElementChild;
-                    a.textContent = feed.title;
-                    a.href = feed.link;
-                    header.style.fontSize = getFontSize(feed);
-                    el.main.appendChild(node);
-                });
-
-                // Remove template node
-                el.main.removeChild(el.article);
+            // Larger fonts for more comments
+            var getFontSize = function (feed) {
+                var size = 1,
+                    unit = 'rem',
+                    extra = feed.comment * 0.1;
+                return size + extra + unit;
             },
 
-            getFeed = function () {
-                var xhr = new XMLHttpRequest();
-                xhr.onload = function () {
-                    var data = xhr.responseText;
-                    renderFeed(data);
-                };
-                xhr.open('GET', URL_FEED);
-                xhr.send();
+                // Append feeds which are sent from server as sorted list
+                feeds = JSON.parse(data);
+
+            feeds.forEach(function (feed) {
+                var article, header, a;
+
+                article = tmpl('article-tmpl');
+                header = article.firstElementChild;
+                a = header.firstElementChild;
+
+                a.textContent = feed.title;
+                a.href = feed.link;
+                header.style = {};
+                header.style.fontSize = getFontSize(feed);
+                el.main.appendChild(article);
+            });
+        };
+
+        getFeed = function () {
+            var xhr = new XMLHttpRequest();
+            xhr.onload = function () {
+                var data = xhr.responseText;
+                renderFeed(data);
             };
+            xhr.open('GET', URL_FEED);
+            xhr.send();
+        };
 
 
         // 1. (TODO) Load contents from local storage
